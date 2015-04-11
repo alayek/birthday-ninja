03ae6f6136f3c59840dd18373a25733530770cff
Arijit <notmyemailid@example.com>
2015-04-11 10:28:19 +0000

    Add comment and likes; though the like is throwing error 400

--- birthday_ninja.py
+++ birthday_ninja.py
@@ -19,8 +19,13 @@ def get_birthday_feed():
       print "(R)eply, (n)ext or (e)xit?"
       user_response = str(raw_input())
       if user_response == 'R' or user_response == 'r':
+        # comment
         comment = comment_list[random.randint(0, len(comment_list)-1)]
         print "Replying With : " + comment
+        r = requests.post("https://graph.facebook.com/" + result['data'][i]['id'].split("_")[1] + "/comments?access_token=" + TOKEN + "&message=" + comment)
+        #like
+        r = requests.post("https://graph.facebook.com/" + result['data'][i]['id'].split("_")[1] + "/likes?access_token=" + TOKEN)
+        print r
         # send a post
       elif user_response == 'n' or user_response == "N":
         print "Skipping this guy"
